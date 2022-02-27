e2643a6f0fc785ee0a3f3d9c171ecef05c172bc6
Vladimir Novikov <novikovvlal@gmail.com>
2022-02-27 13:15:05 +0300

    Increment postfix and prefix

---
+++ 9. DivisionAndIncrement/9. DivisionAndIncrement.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 11;
+    int b = 3;
+    double c = 3;
+
+    cout << a / b;
+    cout << a / c;
+
+    int x = 5;
+    x = x + 2;
+    x += 2;
+    x -= 2;
+    x *= 2;
+    x /= 2;
+
+    x += 1;
+    x++;
+    ++x;
+
+    cout << x;
+
+    int d = x++;
+    int z = ++x;
+
+    cout << endl;
+
+    cout << d << endl;
+    cout << z << endl;
+    
+    return 0;
+}
