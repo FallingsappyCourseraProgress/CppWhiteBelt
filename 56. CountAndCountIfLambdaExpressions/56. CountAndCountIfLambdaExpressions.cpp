e5f0d50f16b69d0e5599fb4c76a4468e96c801e5
Vladimir Novikov <novikovvlal@gmail.com>
2022-03-13 18:17:21 +0300

    Modifying range base for

---
+++ 56. CountAndCountIfLambdaExpressions/56. CountAndCountIfLambdaExpressions.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool Gt2(int x)
+{
+    return x > 2;
+}
+
+bool Lt2(int x)
+{
+    return x < 2;
+}
+
+int main()
+{
+    vector<int> v{ 1, 3, 2, 5, 4, 2 };
+
+    int numberOfOccurances = 0;
+    for (auto i : v)
+    {
+        if (i == 2)
+        {
+            numberOfOccurances++;
+        }
+    }
+
+    cout << count(begin(v), end(v), 2) << endl;
+    cout << count_if(begin(v), end(v), Gt2) << endl;
+    cout << count_if(begin(v), end(v), Lt2) << endl;
+
+    cout << count_if(begin(v), end(v), [](int x) { return x > 2; }) << endl;
+
+    int thr = 0;
+    cin >> thr;
+
+    cout << count_if(begin(v), end(v), [thr](int x) { return thr > 2; }) << endl;
+
+    return 0;
+}
---
+++ 57. ModifyingRangeBaseFor/57. ModifyingRangeBaseFor.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+void Print(const vector<int>& v, const string& title)
+{
+    cout << title << ": " << endl;
+    for (const auto& i : v)
+    {
+        cout << i << " ";
+    }
+}
+
+int main()
+{
+    vector<int> v{ 1, 2, 5, 4, 3 };
+
+    Print(v, "init");
+
+    cout << endl;
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        ++v[i];
+    }
+
+    Print(v, "inc");
+
+    for (auto& i : v)
+    {
+        ++i;
+    }
+
+    Print(v, "inc");
+
+    return 0;
+}
