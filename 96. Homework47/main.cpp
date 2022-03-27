e815dae6e8c2af8d37d3a56f80238a98bf01eb6c
Vladimir Novikov <novikovvlal@gmail.com>
2022-03-27 23:35:02 +0300

    Final task begin

---
+++ 96. Homework47/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <exception>
+#include <string>
+using namespace std;
+
+string AskTimeServer() {
+    /* Для тестирования повставляйте сюда код, реализующий различное поведение этой функии:
+       * нормальный возврат строкового значения
+       * выброс исключения system_error
+       * выброс другого исключения с сообщением.
+    */
+}
+
+class TimeServer {
+public:
+    string GetCurrentTime() {
+        try
+        {
+            auto currentTime = AskTimeServer();
+
+            if (!currentTime.empty())
+            {
+                last_fetched_time = currentTime;
+
+                return last_fetched_time;
+            }
+        }
+        catch (system_error& e)
+        {
+            return last_fetched_time;
+        }
+        catch (const std::exception&)
+        {
+            throw;
+        }
+    }
+
+private:
+    string last_fetched_time = "00:00:00";
+};
+
+int main() {
+    // Меняя реализацию функции AskTimeServer, убедитесь, что это код работает корректно
+    TimeServer ts;
+    try {
+        cout << ts.GetCurrentTime() << endl;
+    }
+    catch (exception& e) {
+        cout << "Exception got: " << e.what() << endl;
+    }
+    return 0;
+}
